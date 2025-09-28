7d4b84b766524318138c1eccc0a363669bcfec10
Magdiel Campelo <magdiel.camp@gmail.com>
2025-09-28 16:03:09 +0100

    chore: begin project

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="bg-isabelline-500 dark:bg-ultra-violet-500 w-screen h-screen flex flex-col overflow-auto">
+    <slot />
+  </main>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+  ],
+})
